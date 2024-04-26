513060dd82b590f0734f20d89555ba75b662ca5c
durbanlegend <dforbes@iafrica.com>
2024-04-26 23:33:44 +0200

    Macro and optimised enum for colour printing

--- src/term_colors.rs
+++ src/term_colors.rs
@@ -1,25 +1,35 @@
 /*[toml]
 [dependencies]
-convert_case = "0.6.0"
 owo-colors = { version = "4.0.0", features = ["supports-colors"] }
 strum = { version = "0.26.2", features = ["derive"] }
 termbg = "0.5.0"
 */
 
-use convert_case::{Case, Casing};
 use owo_colors::colors::css::{Black, DarkOrange, Orange};
 use owo_colors::colors::{Blue, Cyan, Green, Red, White};
 use owo_colors::{OwoColorize, Style};
 use strum::{EnumIter, IntoEnumIterator, IntoStaticStr};
 use termbg::Theme;
 
+/// A version of println that prints an entire message in colour or otherwise styled.
+/// Format: `color_println`!(<style>, "Lorem ipsum dolor {} amet", <content>);
+#[macro_export]
+macro_rules! color_println {
+    ($style:expr, $($arg:tt)*) => {{
+        let binding = format!("{}", format_args!($($arg)*));
+        let styled_text = binding.style($style);
+        println!("{}", styled_text);
+    }};
+}
+
 pub trait ThemeStyle {
-    fn get_style(&self) -> Style;
+    fn get_style(&self, theme: Theme) -> Style;
+    fn to_string(&self) -> String;
 }
 
-// Enum for light theme styles
+// Enum for light and dark theme styles
 #[derive(Clone, Copy, EnumIter, IntoStaticStr)]
-pub enum LightStyle {
+pub enum YinYangStyle {
     Error,
     Warning,
     Emphasis,
@@ -27,80 +37,66 @@ pub enum LightStyle {
     Debug,
 }
 
-// Enum for dark theme styles
-#[derive(Clone, Copy, EnumIter, IntoStaticStr)]
-pub enum DarkStyle {
-    Error,
-    Warning,
-    Emphasis,
-    Info,
-    Debug,
-}
-
-impl ThemeStyle for LightStyle {
+impl ThemeStyle for YinYangStyle {
     // Get the corresponding color style for the message type
-    fn get_style(&self) -> Style {
-        match *self {
-            LightStyle::Error => Style::new().fg::<Red>().bold(),
-            LightStyle::Warning => Style::new().fg::<DarkOrange>().bold(),
-            LightStyle::Emphasis => Style::new().fg::<Green>().bold(),
-            LightStyle::Info => Style::new().fg::<Black>(),
-            LightStyle::Debug => Style::new().fg::<Blue>(),
+    fn get_style(&self, theme: Theme) -> Style {
+        match theme {
+            Theme::Light => match *self {
+                YinYangStyle::Error => Style::new().fg::<Red>().bold(),
+                YinYangStyle::Warning => Style::new().fg::<DarkOrange>().bold(),
+                YinYangStyle::Emphasis => Style::new().fg::<Green>().bold(),
+                YinYangStyle::Info => Style::new().fg::<Black>(),
+                YinYangStyle::Debug => Style::new().fg::<Blue>(),
+            },
+            Theme::Dark => match *self {
+                YinYangStyle::Error => Style::new().fg::<Red>().bold(),
+                YinYangStyle::Warning => Style::new().fg::<Orange>().bold(),
+                YinYangStyle::Emphasis => Style::new().fg::<Green>().bold(),
+                YinYangStyle::Info => Style::new().fg::<White>(),
+                YinYangStyle::Debug => Style::new().fg::<Cyan>(),
+            },
         }
     }
-}
 
-impl ThemeStyle for DarkStyle {
-    // Get the corresponding color style for the message type
-    fn get_style(&self) -> Style {
+    fn to_string(&self) -> String {
         match *self {
-            DarkStyle::Error => Style::new().fg::<Red>().bold(),
-            DarkStyle::Warning => Style::new().fg::<Orange>().bold(),
-            DarkStyle::Emphasis => Style::new().fg::<Green>().bold(),
-            DarkStyle::Info => Style::new().fg::<White>(),
-            DarkStyle::Debug => Style::new().fg::<Cyan>(),
+            YinYangStyle::Error => String::from("error"),
+            YinYangStyle::Warning => String::from("warning"),
+            YinYangStyle::Emphasis => String::from("emphasis"),
+            YinYangStyle::Info => String::from("info"),
+            YinYangStyle::Debug => String::from("debug"),
         }
     }
 }
 
 #[allow(dead_code)]
 fn main() {
-    let timeout = std::time::Duration::from_millis(100);
-
-    println!("Check terminal background color");
     let term = termbg::terminal();
-    let theme: Result<Theme, termbg::Error> = termbg::theme(timeout);
+
+    let theme = get_theme();
 
     println!("  Term : {:?}", term);
 
-    let supports_color: bool = match theme {
-        Ok(_theme) => true,
-        Err(ref _e) => false,
-    };
-
     // Get the appropriate style based on the theme
-    if supports_color {
-        match theme.unwrap() {
-            Theme::Light => {
-                for variant in LightStyle::iter() {
-                    let level: &str =
-                        &<LightStyle as Into<&str>>::into(variant).to_case(Case::Kebab);
-                    let msg = &format!("My {} message", level);
-                    println!("{}", msg.style(variant.get_style()));
-                }
-            }
-
-            // <Opt as Into<&'static str>>::into(option).to_case(Case::Kebab)
-            Theme::Dark => {
-                for variant in DarkStyle::iter() {
-                    let level: &str =
-                        &<DarkStyle as Into<&str>>::into(variant).to_case(Case::Kebab);
-                    let msg = &format!("My {} message", level);
-                    println!("{}", msg.style(variant.get_style()));
-                }
-            }
+    for variant in YinYangStyle::iter() {
+        let level: &str = &variant.to_string();
+        let borrowed_theme = theme.as_ref();
+        if let Ok(theme_ref) = borrowed_theme {
+            color_println!(
+                variant.get_style(*theme_ref),
+                "{}",
+                format!("My {theme_ref:?} style {} message", level)
+            );
+        } else {
+            println!("My warning message - no color support");
         }
-    } else {
-        println!("My warning message - no color support");
     }
+}
+
+fn get_theme() -> Result<Theme, termbg::Error> {
+    let timeout = std::time::Duration::from_millis(100);
+
+    println!("Check terminal background color");
+    let theme: Result<Theme, termbg::Error> = termbg::theme(timeout);
+    theme
 }

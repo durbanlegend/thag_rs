b91e4085a0f6e24ac0cc8e1fd0b0f6f4a421c849
durbanlegend <dforbes@iafrica.com>
2024-05-06 15:13:25 +0200

    Add Hinter

--- examples/reedline_matching_bracket_validator.rs
+++ examples/reedline_matching_bracket_validator.rs
@@ -6,12 +6,17 @@ reedline = "0.32.0"
 
 // Pressing [Enter] will in other cases give you a multi-line prompt.
 
-use reedline::{DefaultPrompt, DefaultValidator, Reedline, Signal};
+use nu_ansi_term::{Color, Style};
+use reedline::{DefaultHinter, DefaultPrompt, DefaultValidator, Reedline, Signal};
 use std::io;
 
 fn main() -> io::Result<()> {
     println!("Enter a dummy expression to evaluate. Expressions in matching braces, brackets or quotes may span multiple lines.\nAbort with Ctrl-C or Ctrl-D");
-    let mut line_editor = Reedline::create().with_validator(Box::new(DefaultValidator));
+    let mut line_editor = Reedline::create()
+        .with_validator(Box::new(DefaultValidator))
+        .with_hinter(Box::new(
+            DefaultHinter::default().with_style(Style::new().italic().fg(Color::LightGray)),
+        ));
 
     let prompt = DefaultPrompt::default();
 

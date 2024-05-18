dec2a0d09c82e5e49aa60d275c542cff4df913d9
durbanlegend <dforbes@iafrica.com>
2024-05-18 14:22:51 +0200

    Debug termbg crate

---
+++ examples/termbg_bug.rs
@@ -0,0 +1,37 @@
+/*[toml]
+[dependencies]
+termbg = "0.5.0"
+*/
+
+use std::io::{self, Read};
+
+fn main() {
+    let timeout = std::time::Duration::from_millis(100);
+
+    println!("Check terminal background color");
+    // let term = termbg::terminal();
+    let rgb = termbg::rgb(timeout);
+    // let theme = termbg::theme(timeout);
+
+    let mut buffer = String::new();
+    io::stdin().lock().read_to_string(&mut buffer).unwrap();
+    println!("buffer={buffer:?}");
+
+    // match rgb {
+    //     Ok(rgb) => {
+    //         println!("  Color: R={:x}, G={:x}, B={:x}", rgb.r, rgb.g, rgb.b);
+    //     }
+    //     Err(e) => {
+    //         println!("  Color: detection failed {:?}", e);
+    //     }
+    // }
+
+    // match theme {
+    //     Ok(theme) => {
+    //         println!("  Theme: {:?}", theme);
+    //     }
+    //     Err(e) => {
+    //         println!("  Theme: detection failed {:?}", e);
+    //     }
+    // }
+}

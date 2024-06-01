887518242b0ac4ebcd99b74cc3adcb4c8d30b651
durbanlegend <dforbes@iafrica.com>
2024-06-01 09:39:11 +0200

    Update rs_script.rs

--- src/bin/rs_script.rs
+++ src/bin/rs_script.rs
@@ -7,15 +7,13 @@ use std::error::Error;
 //       1.  Consider supporting alternative TOML embedding keywords so we can run demo/regex_capture_toml.rs.
 //       2.  Consider history support for stdin.
 //       3.  Paste event in Windows slow or not happening?
-//       4.  TUI editor as an option on stdin.
-//       5.  How to navigate reedline history entry by entry instead of line by line.
+//       4.  How to navigate reedline history entry by entry instead of line by line.
 //       6.  How to insert line feed from keyboard to split line in reedline. (Supposedly shift+enter)
 //       7.  More unit and integration tests
 //       8.  Cat files before delete.
 //       9.  Decide if it's worth passing the wrapped syntax tree to gen_build_run from eval just to avoid
 //           re-parsing it for that specific use case.
-//  >>> 10.  cmd_args.rs unit tests per ChatGPT.
-//      11.  Clean up debugging
+//  >>> 10.  Clean up debugging
 //      12.  "edit" crate - how to reconfigure editors dynamically - instructions unclear.
 //      13.  Clap aliases not working in REPL.
 //      14.  Get rid of date and time in RHS of REPL? - doesn't seem to be an option.

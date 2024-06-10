d89b8722221ca8607df0e4eb09ac4815a81523d6
durbanlegend <dforbes@iafrica.com>
2024-06-10 09:26:32 +0200

    Make TODO.md

---
+++ TODO.md
@@ -0,0 +1,28 @@
+# TODO List
+
+## High Priority
+- [ ]  Clean up demos and additional popular crates
+
+## Medium Priority
+- [ ]  Document public APIs
+- [ ]  More unit and integration tests
+- [ ]  Clean up debugging
+
+## Low Priority
+- [ ]  Consider history support for stdin.
+- [ ]  Paste event in Windows slow or not happening?
+- [ ]  How to insert line feed from keyboard to split line in reedline. (Supposedly shift+enter)
+- [ ]  Decide if it's worth passing the wrapped syntax tree to gen_build_run from eval just to avoid re-parsing it for that specific use case.
+- [ ]  "edit" crate - how to reconfigure editors dynamically - instructions unclear.
+- [ ]  Clap aliases not working in REPL.
+- [ ]  Get rid of date and time in RHS of REPL? - doesn't seem to be an option.
+- [ ]  Help command in eval, same as quit and q
+- [ ]  Work on demo/reedline_clap_repl_gemini.rs
+- [ ]  Consider other Rust gui packages.
+- [ ]  How to navigate reedline history entry by entry instead of line by line.
+
+
+## Ideas / Future Enhancements
+- [ ]  Consider supporting alternative TOML embedding keywords so we can run demo/regex_capture_toml.rs.
+- [ ]  Option to cat files before delete.
+- [ ]  WASM - is there a worthwhile one? - maybe Leptos if it doesn't need Node.js.
--- src/bin/rs_script.rs
+++ src/bin/rs_script.rs
@@ -3,29 +3,6 @@
 use rs_script::{execute, get_args};
 use std::error::Error;
 
-//      TODO:
-//       1.  Consider supporting alternative TOML embedding keywords so we can run demo/regex_capture_toml.rs.
-//       2.  Consider history support for stdin.
-//       3.  Paste event in Windows slow or not happening?
-//       4.  How to navigate reedline history entry by entry instead of line by line.
-//       6.  How to insert line feed from keyboard to split line in reedline. (Supposedly shift+enter)
-//       7.  More unit and integration tests
-//       8.  Cat files before delete.
-//       9.  Decide if it's worth passing the wrapped syntax tree to gen_build_run from eval just to avoid
-//           re-parsing it for that specific use case.
-//      10.  Clean up debugging
-//  >>> 11.  Replace dbg by something less funky - or maybe a CLI option
-//      12.  "edit" crate - how to reconfigure editors dynamically - instructions unclear.
-//      13.  Clap aliases not working in REPL.
-//      14.  Get rid of date and time in RHS of REPL? - doesn't seem to be an option.
-//      15.  Help command in eval, same as quit and q
-//      16.  Work on demo/reedline_clap_repl_gemini.rs
-//      17.  Put the more intractable long-term problems here in a separate TODO file?
-//      18.  How to set editor in Windows.
-//      19.  Consider other Rust gui packages.
-//      20.  Debug Cargo search happening for package defined with mod - AST OK, still to test source-regex example
-//      21.  Find a WASM demo - try trunk vanilla rust example generating html?
-
 pub fn main() -> Result<(), Box<dyn Error>> {
     let args = get_args();
     execute(args)?;

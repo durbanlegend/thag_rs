be417ea9bd29ff7b670d6fafa282078feead1c8c
durbanlegend <dforbes@iafrica.com>
2024-07-09 09:52:52 +0200

    Create reedline_colored_text.rs

---
+++ demo/reedline_colored_text.rs
@@ -0,0 +1,40 @@
+/*[toml]
+[dependencies]
+crossterm = "0.27.0"
+reedline = "0.32.0"
+*/
+
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetForegroundColor},
+};
+use reedline::{DefaultPrompt, Reedline, Signal};
+use std::io::{stdout, Write};
+
+fn main() {
+    let mut line_editor = Reedline::create();
+    let prompt = DefaultPrompt::default();
+
+    loop {
+        let sig = line_editor.read_line(&prompt);
+        match sig {
+            Ok(Signal::Success(buffer)) => {
+                print_colored_text(&buffer, Color::Green);
+            }
+            Ok(Signal::CtrlC) => {
+                break;
+            }
+            Ok(Signal::CtrlD) => {
+                line_editor.clear_screen().unwrap();
+            }
+            Err(err) => {
+                eprintln!("Error: {:?}", err);
+            }
+        }
+    }
+}
+
+fn print_colored_text(text: &str, color: Color) {
+    let mut stdout = stdout();
+    execute!(stdout, SetForegroundColor(color), Print(text), ResetColor).unwrap();
+}

21fd645d72642e3dc7f35cdab90fcef2f7020cb9
durbanlegend <dforbes@iafrica.com>
2024-07-09 16:25:57 +0200

    Track down fizz-buzz origin
    "In general, if example code is offered with this book, you  may use it in your programs and documentation. You do not need to contact us for permission unless you’re reproducing a significant portion of the code."

---
+++ demo/fizz_buzz_blandy_orendorff.rs
@@ -0,0 +1,18 @@
+/// A fun example from Programming Rust by Jim Blandy and Jason Orendorff (O’Reilly).
+/// Copyright 2018 Jim Blandy and Jason Orendorff, 978-1-491-92728-1.
+/// Described by the authors as "a really gratuitous use of iterators".
+//# Purpose: Demo using `rs-script` to try out random code snippets ... also iterators.
+use std::iter::{once, repeat};
+
+let fizzes = repeat("").take(2).chain(once("fizz")).cycle();
+let buzzes = repeat("").take(4).chain(once("buzz")).cycle();
+let fizzes_buzzes = fizzes.zip(buzzes);
+
+let fizz_buzz = (1..100).zip(fizzes_buzzes).map(|tuple| match tuple {
+    (i, ("", "")) => i.to_string(),
+    (_, (fizz, buzz)) => format!("{}{}", fizz, buzz),
+});
+
+for line in fizz_buzz {
+    println!("{}", line);
+}

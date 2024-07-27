9d59b42f7610bebe490bdddf6188e5230f9e273a
durbanlegend <dforbes@iafrica.com>
2024-07-27 14:31:35 +0200

    Demo scripts

--- demo/termbg.rs
+++ demo/termbg.rs
@@ -1,7 +1,34 @@
 /*[toml]
 [dependencies]
+crossterm = "0.27.0"
 termbg = "0.5.0"
 */
+
+/// Published example from `termbg` readme, only I've added the `clear_screen` method
+/// because `termbg` messes with the terminal settings. Obviously that doesn't matter
+/// in a demo that exists before doing serious work, but having struggled with a
+/// persistent issue of rightward drift in println! output that turned out to be
+/// caused by an overlooked termbg call buried in the message colour resolution logic,
+/// I think it's important to make it a habit.
+///
+/// Detects the light or dark theme in use, as well as the colours in use.
+//# Demo theme detection with `termbg` and clearing terminal state with `crossterm`.
+use crossterm::{
+    cursor::{MoveTo, Show},
+    terminal::{Clear, ClearType},
+    ExecutableCommand,
+};
+use std::io::{stdout, Write};
+
+// termbg sends an operating system command (OSC) to interrogate the screen
+// but with side effects which we undo here.
+pub fn clear_screen() {
+    let mut out = stdout();
+    out.execute(Clear(ClearType::All)).unwrap();
+    out.execute(MoveTo(0, 0)).unwrap();
+    out.execute(Show).unwrap();
+    out.flush().unwrap();
+}
 
 fn main() {
     let timeout = std::time::Duration::from_millis(100);
@@ -10,6 +37,7 @@ fn main() {
     let term = termbg::terminal();
     let rgb = termbg::rgb(timeout);
     let theme = termbg::theme(timeout);
+    clear_screen();
 
     println!("  Term : {:?}", term);
 

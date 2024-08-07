ebf65b3049ac911040923aaed28e1fa28f577329
durbanlegend <dforbes@iafrica.com>
2024-08-07 18:22:59 +0200

    Revert to trusting repo instead of my local path

--- demo/stdin.rs
+++ demo/stdin.rs
@@ -1,6 +1,6 @@
 /*[toml]
 [dependencies]
-rs-script = { path = "/Users/donf/projects/rs-script" }
+rs-script = { git = "https://github.com/durbanlegend/rs-script" }
 
 lazy_static = "1.4.0"
 regex = "1.10.4"
--- demo/verbosity.rs
+++ demo/verbosity.rs
@@ -1,6 +1,6 @@
 /*[toml]
 [dependencies]
-rs-script = { path = "/Users/donf/projects/rs-script" }
+rs-script = { git = "https://github.com/durbanlegend/rs-script" }
 
 ratatui = "=0.26.3"
 # [patch.crates-io]

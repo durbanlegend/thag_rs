8ad16edf00f102bf5001d3de1330f6bf9c32aabf
durbanlegend <dforbes@iafrica.com>
2024-08-29 21:36:03 +0200

    Attack on the clone()s: optimisations
    Caution: a few minor logic changes now consuming instead of cloning.
    Distinguish between choosing no colour (= None) and not having made a choice (= Default).

--- src/repl.rs
+++ src/repl.rs
@@ -184,6 +184,7 @@ pub fn run_repl(
     };
     // get_emacs_keybindings();
     let context: &mut Context = &mut context;
+    let history_file = context.build_state.cargo_home.join(HISTORY_FILE);
     let history = Box::new(
         FileBackedHistory::with_file(25, history_file)
             .expect("Error configuring history with file"),

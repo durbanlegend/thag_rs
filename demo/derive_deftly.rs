e1a25f617fa52e6971fc438165d08e809ba04ac6
durbanlegend <dforbes@iafrica.com>
2024-11-13 00:14:13 +0200

    derive-deftly examples

---
+++ demo/derive_deftly.rs
@@ -0,0 +1,25 @@
+/*[toml]
+[dependencies]
+derive-deftly = "0.14.2"
+*/
+/// Introductory example from the `derive-deftly` user guide.
+//# Purpose: Explore proc macro alternatives.
+use derive_deftly::define_derive_deftly;
+
+define_derive_deftly! {
+    HelloWorld:
+
+    impl $ttype {
+        pub fn greet() {
+            println!("Greetings from {}", stringify!($ttype));
+        }
+    }
+}
+
+use derive_deftly::Deftly;
+
+#[derive(Clone, Debug, Deftly)]
+#[derive_deftly(HelloWorld)]
+pub struct MyStruct;
+
+MyStruct::greet();

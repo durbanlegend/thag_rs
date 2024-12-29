5e73faffade99ed253f3d49c1509db8b1aef61ff
durbanlegend <dforbes@iafrica.com>
2024-12-29 22:12:58 +0200

    Remove embed proc macros & add include_str! demo
    Could not find a straightforward way to embed because thag generated cargo home is under temp_dir(), with no link back to source demo dir.

---
+++ demo/include_str.rs
@@ -0,0 +1,11 @@
+/// Simple demo of `std::include_str` macro showing how to includes other files in demo or neighboring
+/// directories.
+///
+/// This requires a main method so that `thag` won't move the snippet to a location under temp_dir().
+//# Purpose: demo technique
+//# Categories: basic, educational, technique
+fn main() {
+    let my_str = include_str!("hello.rs");
+    assert_eq!(my_str, "//: Obligatory Hello World as a snippet\n//# Purpose: Demo Hello World snippet\n//# Categories: basic\nlet other = \"World\";\nprintln!(\"Hello, {other}!\");\n");
+    print!("{my_str}");
+}

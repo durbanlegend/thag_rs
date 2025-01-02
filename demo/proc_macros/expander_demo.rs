7cc168442c77adcc71bba96d26f1d4e11cc8f4d1
durbanlegend <dforbes@iafrica.com>
2025-01-02 20:32:12 +0200

    Expander demo: use branch revised_pr of my fork.

--- demo/proc_macros/expander_demo.rs
+++ demo/proc_macros/expander_demo.rs
@@ -12,7 +12,7 @@ pub fn baz2(input: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
         .add_comment("This is generated code!".to_owned())
         // Formatting seems to be getting OS error 33 sometimes on Windows:
         // "error: couldn't read \\?\C:\Users\runneradmin\AppData\Local\Temp\thag_rs\proc_macro_expander_demo\target\debug\build\expander-29b18d460c115a5a\out\baz_0-3535fb96b397.rs: The process cannot access the file because another process has locked a portion of the file. (os error 33)"
-        // .fmt(Edition::_2021)
+        .fmt(Edition::_2021)
         .verbose(true)
         // common way of gating this, by making it part of the default feature set
         // .dry(cfg!(feature = "no-file-expansion"))

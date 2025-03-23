fd93209571d4c7b2a5dc0d6a7c296026ab34b332
durbanlegend <dforbes@iafrica.com>
2025-03-23 08:30:01 +0200

    Update allocation_test.rs

--- thag_profiler/tests/allocation_test.rs
+++ thag_profiler/tests/allocation_test.rs
@@ -4,14 +4,14 @@ use thag_profiler::task_allocator::create_memory_task;
 fn test_basic_allocation() {
     // Create a memory task
     let task = create_memory_task();
-    
+
     // Do some allocations
-    let data1: Vec<u8> = vec\![0; 1024];
-    let data2: Vec<u8> = vec\![0; 2048];
-    
+    let data1: Vec<u8> = vec![0; 1024];
+    let data2: Vec<u8> = vec![0; 2048];
+
     // Verify we can query memory usage without a crash
-    println\!("Memory usage: {:?}", task.memory_usage());
-    
+    println!("Memory usage: {:?}", task.memory_usage());
+
     // Keep data in scope
-    println\!("Data sizes: {}, {}", data1.len(), data2.len());
+    println!("Data sizes: {}, {}", data1.len(), data2.len());
 }

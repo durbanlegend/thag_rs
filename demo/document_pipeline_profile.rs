83975b869604616980fc67e25fe90fe4f33c4afd
durbanlegend <dforbes@iafrica.com>
2025-03-26 12:00:24 +0200

    Update document_pipeline_profile.rs

--- demo/document_pipeline_profile.rs
+++ demo/document_pipeline_profile.rs
@@ -1,8 +1,8 @@
 /*[toml]
 [dependencies]
-thag_profiler = { git = "https://github.com/durbanlegend/thag_rs", branch = "develop", features = ["full_profiling"] }
+# thag_profiler = { git = "https://github.com/durbanlegend/thag_rs", branch = "develop", features = ["full_profiling"] }
 # thag_profiler = { version = "0.1", features = ["full_profiling"] }
-# thag_profiler = { path = "/Users/donf/projects/thag_rs/thag_profiler", features = ["full_profiling"] }
+thag_profiler = { path = "/Users/donf/projects/thag_rs/thag_profiler", features = ["full_profiling"] }
 */
 
 use futures::future::join_all;

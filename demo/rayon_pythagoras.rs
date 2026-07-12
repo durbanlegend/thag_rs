6dafd7c7f8f3b02a753c6099ddc55c64543e07ad
durbanlegend <dforbes@iafrica.com>
2026-07-12 17:13:41 +0200

    Update rayon_pythagoras.rs
    Fix the stated limit, add some displays and explanations, minimise risk of finger trouble.

--- demo/rayon_pythagoras.rs
+++ demo/rayon_pythagoras.rs
@@ -2,8 +2,8 @@
 /// https://github.com/rayon-rs/rayon/blob/main/rayon-demo/src/pythagoras/mod.rs
 //# Purpose: Demo the featured crate and the performance effect of using `rayon`.
 //# Categories: crates, performance, timing
-// How many Pythagorean triples exist less than or equal to a million?
-// i.e. a²+b²=c² and a,b,c ≤ 1000000
+// How many Pythagorean triples exist less than or equal to 4 million?
+// i.e. a²+b²=c² and a,b,c ≤ 4000000
 use num::Integer;
 use rayon::prelude::*;
 use rayon::range::Iter;
@@ -20,33 +20,38 @@ use std::time::Duration;
 
 use thag_proc_macros::timing;
 
+const MAX_M: u32 = 2_000;
+const MAX_C: u32 = MAX_M * MAX_M;
+
 #[timing]
 /// Without using rayon.
 fn euclid() -> u32 {
-    (1u32..2_000)
+    (1u32..MAX_M)
         .map(|m| -> u32 {
             (1..m)
                 .filter(|n| (m - n).is_odd() && m.gcd(n) == 1)
-                .map(|n| 4_000_000 / (m * m + n * n))
-                .sum()
+                .map(|n| MAX_C / (m * m + n * n)) // Number of integer multipliers k that can create eligible triples from this primary triple pair (m,n)
+                .sum() // Over this m and all eligible n
         })
-        .sum()
+        .sum() // Over all eligible m, n
 }
 
 #[timing]
 /// Using rayon
 fn par_euclid_weightless() -> u32 {
-    (1u32..2_000)
+    (1u32..MAX_M)
         .into_par_iter()
         .map(|m| -> u32 {
             (1..m)
                 .into_par_iter()
                 .filter(|n| (m - n).is_odd() && m.gcd(n) == 1)
-                .map(|n| 4_000_000 / (m * m + n * n))
+                .map(|n| MAX_C / (m * m + n * n))
                 .sum()
         })
         .sum()
 }
 
-par_euclid_weightless();
-euclid();
+println!("How many Pythagorean triples exist less than or equal to 4 million?");
+println!("i.e. a²+b²=c² and a,b,c ≤ {MAX_C}");
+println!("rayon calculation gives {}.", par_euclid_weightless());
+println!("Regular calculation gives {}.", euclid());
